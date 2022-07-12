3750324cb8a0a7ec0965cd02f5f8cf0f9ba2a625
ndrklmr <andrekolmeijer@gmail.com>
2022-07-12 13:58:43 +0200

    fix: src in DataStore

--- src/stores/DataStore.js
+++ src/stores/DataStore.js
@@ -3,38 +3,38 @@ import { defineStore } from 'pinia'
 export const useDataStore = defineStore('DataStore', {
   state: () => ({
     images: [
-      { src: 'src/assets/img/carousel-1.jpg', span: 'col-span-4 row-span-2', alt: 'Twee maal Dark & Stormy cocktail' },
+      { src: '@/assets/img/carousel-1.jpg', span: 'col-span-4 row-span-2', alt: 'Twee maal Dark & Stormy cocktail' },
       {
-        src: 'src/assets/img/carousel-2.jpg',
+        src: '@/assets/img/carousel-2.jpg',
         span: 'col-span-3 row-span-2',
         alt: 'Twee White Russians die bereid worden'
       },
       {
-        src: 'src/assets/img/carousel-3.jpg',
+        src: '@/assets/img/carousel-3.jpg',
         span: 'col-span-5 row-span-2',
         alt: 'Jehan en Rob maken Espresso Martini'
       },
       {
-        src: 'src/assets/img/carousel-4.jpg',
+        src: '@/assets/img/carousel-4.jpg',
         span: 'col-span-4 row-span-2',
         alt: 'Inspire specialiteit C-star met kruidnoten'
       },
       {
-        src: 'src/assets/img/carousel-5.jpg',
+        src: '@/assets/img/carousel-5.jpg',
         span: 'col-span-4 row-span-2',
         alt: 'Een yogurt, lemon and lime swirl cake'
       },
-      { src: 'src/assets/img/carousel-6.jpg', span: 'col-span-4 row-span-1', alt: 'Bovenverdieping Inspire Breda' },
-      { src: 'src/assets/img/carousel-7.jpg', span: 'col-span-2 row-span-1', alt: 'Martini Espresso van bovenaf' },
-      { src: 'src/assets/img/carousel-8.jpg', span: 'col-span-2 row-span-1', alt: 'Verse appeltaart' }
+      { src: '@/assets/img/carousel-6.jpg', span: 'col-span-4 row-span-1', alt: 'Bovenverdieping Inspire Breda' },
+      { src: '@/assets/img/carousel-7.jpg', span: 'col-span-2 row-span-1', alt: 'Martini Espresso van bovenaf' },
+      { src: '@/assets/img/carousel-8.jpg', span: 'col-span-2 row-span-1', alt: 'Verse appeltaart' }
     ],
     menukaarten: [
-      { href: '/menu/lunch-1.pdf', src: 'src/assets/img/lunch-1.jpg', name: 'Lunchkaart' },
-      { href: '/menu/drank-1.pdf', src: 'src/assets/img/drank-1.jpg', name: 'Koffie & Warm' },
-      { href: '/menu/drank-2.pdf', src: 'src/assets/img/drank-2.jpg', name: 'Sap & Frisdrank' },
-      { href: '/menu/drank-3.pdf', src: 'src/assets/img/drank-3.jpg', name: 'Bierkaart' },
-      { href: '/menu/drank-4.pdf', src: 'src/assets/img/drank-4.jpg', name: 'Wijn & Bubbels' },
-      { href: '/menu/drank-5.pdf', src: 'src/assets/img/drank-5.jpg', name: 'Cocktails' }
+      { href: '/menu/lunch-1.pdf', src: '@/assets/img/lunch-1.jpg', name: 'Lunchkaart' },
+      { href: '/menu/drank-1.pdf', src: '@/assets/img/drank-1.jpg', name: 'Koffie & Warm' },
+      { href: '/menu/drank-2.pdf', src: '@/assets/img/drank-2.jpg', name: 'Sap & Frisdrank' },
+      { href: '/menu/drank-3.pdf', src: '@/assets/img/drank-3.jpg', name: 'Bierkaart' },
+      { href: '/menu/drank-4.pdf', src: '@/assets/img/drank-4.jpg', name: 'Wijn & Bubbels' },
+      { href: '/menu/drank-5.pdf', src: '@/assets/img/drank-5.jpg', name: 'Cocktails' }
     ],
     openingstijden: [
       { dag: 'Maandag', tijd: '09:00 - 17:00*' },
