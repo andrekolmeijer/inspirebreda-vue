5b4ee6bfb033a753e20e69e7ccfacdccb81dd012
Andre Kolmeijer <andrekolmeijer@gmail.com>
2023-04-06 10:30:05 +0200

    feat:  alert banner

---
+++ src/components/WelcomeAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="border rounded border-inspire-orange">
+    <div class="max-w-5xl m-auto group">
+      <a href="#openingstijden" rel="nofollow">
+        <p class="px-4 py-4 text-sm font-light text-center">
+          <!-- Let op: i.v.m. de feestdagen en COVID-19 hanteren wij afwijkende openingstijden. -->
+          We zijn volop bezig met de voorbereidingen van de verbouwing van onze begane grond! <span class="font-normal">Houd de site in de gaten voor updates.</span>
+          <!-- Wij zijn volop bezig met de voorbereidingen van de verbouwing van onze begane grond. De bovenverdieping blijft open! -->
+          <!-- <span class="group-hover:underline">Meer info</span><svg class="h-3 w-3 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg> -->
+        </p>
+      </a>
+    </div>
+  </div>
+</template>
--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -3,10 +3,12 @@ import InstagramCall from './InstagramCall.vue'
 import PhotoGrid from './PhotoGrid.vue'
 import WelcomeText from './WelcomeText.vue'
 import WelcomeBanner from './WelcomeBanner.vue'
+import WelcomeAlert from './WelcomeAlert.vue';
 </script>
 
 <template>
   <section class="m-auto max-w-5xl text-inspire-brown-dark">
+    <WelcomeAlert class="mx-4 sm:mx-8 mb-8" />
     <WelcomeBanner class="sm:mx-8" />
     <WelcomeText class="mx-8 mt-8" />
     <div class="sm:mx-8">
