c5f08663b763cbb629fa98746d2d37b82afafcc4
Andre Kolmeijer <andrekolmeijer@gmail.com>
2023-04-26 17:05:00 +0200

    feat: structured data SpecialAnnouncement verbouwing

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -2,8 +2,8 @@
   <div class="bg-red-500">
     <div class="max-w-5xl m-auto group">
       <a href="#openingstijden" rel="nofollow">
-        <p class="px-4 py-4 text-center text-inspire-white">
-          <span class="font-semibold">Let op:</span> van 1 t/m 17 mei zijn we gesloten i.v.m. de verbouwing van de bagane grond!
+        <p class="px-4 py-4 text-center text-inspire-white text-sm">
+          <span class="font-semibold">Let op:</span> i.v.m. de verbouwing van de begane grond zijn we 1 t/m 17 mei gesloten. Volg onze socials voor updates!
         </p>
       </a>
     </div>
