35b0e6914f13e8c184d36ed97178f707bee71de6
Andre Kolmeijer <andrekolmeijer@gmail.com>
2023-04-28 16:17:11 +0200

    test: adjusted text

--- src/components/WelcomeText.vue
+++ src/components/WelcomeText.vue
@@ -3,14 +3,19 @@
     <h1 class="w-2/3 pt-8 text-3xl font-black uppercase tracking-normal">
       Welkom bij <span class="text-inspire-orange">Inspire</span>
     </h1>
-    <p class="gap-8 py-8 text-base font-light hyphens-auto sm:columns-2">
-      Met uitzicht op de Grote Markt van Breda genieten van de lekkerste koffie,
-      gezonde lunch en afsluiten met een speciaalbiertje of de perfecte
-      cocktail? Inspire heeft het allemaal in huis! Plof neer op onze bank of
-      borrel met vrienden aan onze bartafel. Met de exper&shy;tise van ons
-      personeel weet je zeker dat je het juiste drankje voorgeschoteld krijgt.
-      Liever een dagje rustig werken? Met onze razendsnelle internetverbinding
-      ben je klaar voor elke meeting. Kom gezellig langs en laat je inspireren!
+    <p class="gap-8 py-8 text-base font-light sm:columns-2">
+      Met uitzicht op de Grote Markt van Breda genieten bij onze koffiebar in
+      het centrum van Breda! Inspire is uniek met koffiebonen van Caffènation.
+      Je kunt hier heerlijk genieten van een gezonde lunch en afsluiten met een
+      speciaalbiertje of de perfecte cocktail. Inspire heeft het allemaal in
+      huis! Plof neer op onze Chesterfield-bank of borrel met vrienden aan onze
+      bartafel. Met de expertise van ons personeel weet je zeker dat je het
+      juiste drankje voorgeschoteld krijgt. Liever een dagje studeren en/of
+      werken? Bij Inspire kun je boven productief aan de slag met volle
+      concentratie, we bieden rustige werkplekken met goede verlichting en
+      comfortabele stoelen om het werken zo aangenaam mogelijk te maken.
+      Daarnaast ben je met onze razendsnelle wifi klaar voor elke meeting. Kom
+      gezellig langs en laat je inspireren!
     </p>
   </div>
 </template>
