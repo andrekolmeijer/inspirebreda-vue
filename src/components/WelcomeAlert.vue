e1bc6f2d29104180115f9e7f05c9a43a91ec536c
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-16 21:52:52 +0200

    ref: mod alert message

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -2,8 +2,8 @@
   <div class="bg-red-500">
     <div class="max-w-5xl m-auto group">
       <a href="#openingstijden" rel="nofollow">
-        <p class="px-4 py-4 text-center text-inspire-white text-sm">
-          <span class="font-semibold">Let op:</span> i.v.m. de verbouwing van de begane grond zijn we 1 t/m 17 mei gesloten. Volg onze socials voor updates!
+        <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
+          <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> De nieuwe website is onder constructie en komt eraan. <br>Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span>
         </p>
       </a>
     </div>
