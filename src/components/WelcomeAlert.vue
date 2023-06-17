8803423ebb08e0fe2f54a3ba4a7c78627015d2c7
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-17 20:19:20 +0200

    ref: add link to new url

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="bg-red-500">
+  <div class="bg-[#14343F]">
     <div class="max-w-5xl m-auto group">
-      <a href="#openingstijden" rel="nofollow">
+      <a href="https://debasisbreda.nl/">
         <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
-          <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis!</span> De nieuwe website is onder constructie en komt eraan. <br>Kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span>
+          <span class="font-semibold">Let op:</span> Inspire Coffee Company is nu <span class="font-semibold">De Basis! </span>Ga naar: <span class="underline font-semibold">debasisbreda.nl</span>.<br> En kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span>
         </p>
       </a>
     </div>
