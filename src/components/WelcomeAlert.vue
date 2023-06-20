bd962ea63c4d50a62ff21659d7671ec6ca24b835
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2023-06-20 14:10:05 +0200

    ref: change alert msg

--- src/components/WelcomeAlert.vue
+++ src/components/WelcomeAlert.vue
@@ -3,7 +3,7 @@
     <div class="max-w-5xl m-auto group">
       <a href="https://debasisbreda.nl/">
         <p class="px-4 py-4 text-center text-inspire-white text-sm font-light">
-          <span class="font-semibold">Inspire Coffee Company</span> is nu <span class="font-semibold">De Basis! <br class="sm:hidden"></span>Ga naar <span class="underline font-semibold">debasisbreda.nl</span>.<br class="hidden sm:block"> En kom naar de <span class="font-semibold">officiële opening</span> op <span class="font-semibold">Za. 17 juni</span> om <span class="font-semibold">16:00 uur!</span>
+          <span class="font-semibold">Inspire Coffee Company</span> is nu <span class="font-semibold">De Basis! <br class="sm:hidden"></span>Ga naar <span class="underline font-semibold">debasisbreda.nl</span>.<br class="hidden sm:block">
         </p>
       </a>
     </div>
